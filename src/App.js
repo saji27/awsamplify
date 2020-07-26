60390693a76245ad9211f605fd3e3209afd49f2b
Sajeesh Baskaran <saji27@gmail.com>
2020-07-25 22:18:24 -0400

    Test amplify

--- src/App.js
+++ src/App.js
@@ -8,17 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          IRTT ECE 2003
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     </header>
     </div>
   );
 }
